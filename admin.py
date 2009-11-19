98eb5af9fae98b8a527d3f08539c81db52cedad2
ewheeler <evanmwheeler@gmail.com>
2009-11-19 17:12:18 +0000

    updated admin

--- admin.py
+++ admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin 
 from models import *
 
+class AssessmentInline(admin.TabularInline):
+    model = Assessment
+
 class HealthWorkerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'message_count', 'errors', 'last_seen')
     search_fields = ('first_name', 'last_name', 'alias')
+    inlines = [AssessmentInline, ]
 
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ('code', 'cluster_id', 'household_id', 'age_in_months', 'status', 'last_updated')
+    list_display = ('code', 'cluster_id', 'household_id', 'gender', 'date_of_birth', 'age_in_months', 'age_in_months_from_date_of_birth', 'status', 'last_updated')
     ordering = ['cluster_id']
     date_hierarchy = 'last_updated'
+    inlines = [AssessmentInline, ]
 
 class AssessmentAdmin(admin.ModelAdmin):
     list_display = ('status', 'healthworker', 'patient', 'date')
