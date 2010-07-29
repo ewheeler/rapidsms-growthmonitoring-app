111776670072376994a86537f4f7855ba9e72d7b
ewheeler <evanmwheeler@gmail.com>
2010-07-29 15:16:09 -0400

    removed old models from admin

--- admin.py
+++ admin.py
@@ -7,20 +7,6 @@ from models import *
 class AssessmentInline(admin.TabularInline):
     model = Assessment
 
-class HealthWorkerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'interviewer_id', 'errors',\
-        'connection', 'last_seen')
-    search_fields = ('first_name', 'last_name', 'alias', 'connection')
-    inlines = [AssessmentInline, ]
-
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('code', 'cluster_id', 'household_id', 'gender',\
-        'date_of_birth', 'age_in_months', 'age_in_months_from_date_of_birth',\
-        'status', 'last_updated')
-    ordering = ['cluster_id']
-    date_hierarchy = 'last_updated'
-    inlines = [AssessmentInline, ]
-
 class AssessmentAdmin(admin.ModelAdmin):
     list_display = ('status', 'healthworker', 'patient', 'date')
     search_fields = ('healthworker', 'patient', 'date', 'status')
@@ -34,8 +20,6 @@ class SurveyAdmin(admin.ModelAdmin):
     list_display = ('location', 'begin_date', 'end_date')
     seach_fields = ('begin_date', 'end_date', 'location', 'description')
 
-admin.site.register(HealthWorker, HealthWorkerAdmin)
-admin.site.register(Patient, PatientAdmin)
 admin.site.register(Assessment, AssessmentAdmin)
 admin.site.register(SurveyEntry, SurveyEntryAdmin)
 admin.site.register(Survey, SurveyAdmin)
