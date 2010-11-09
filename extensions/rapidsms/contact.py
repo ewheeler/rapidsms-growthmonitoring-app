d0a0969c65dc7b6c40dfb476220b78cf63b1e524
ewheeler <evanmwheeler@gmail.com>
2010-11-09 14:00:11 -0500

    added missing fields from reporter

--- extensions/rapidsms/contact.py
+++ extensions/rapidsms/contact.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
+import re
 from django.db import models
 
 
@@ -16,8 +17,10 @@ class HealthWorker(models.Model):
     status                 = models.CharField(max_length=1,choices=HW_STATUS_CHOICES,default='A')
     interviewer_id          = models.PositiveIntegerField(max_length=10, blank=True, null=True)
 
-    def __unicode__(self):
-        return "%s" % (self.name)
+    alias      = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name  = models.CharField(max_length=30, blank=True)
+
 
     def num_messages_sent(self, when=None):
         time_chunks = {
@@ -31,3 +34,60 @@ class HealthWorker(models.Model):
         else:
             return self.incoming_messages.all().count()
 
+    @classmethod
+    def parse_name(klass, flat_name):
+        """Given a single string, this function returns a three-string
+           tuple containing a suggested alias, first name, and last name,
+           via some quite crude pattern matching."""
+
+        patterns = [
+            # try a few common name formats.
+            # this is crappy but sufficient
+            r"([a-z]+)",                       # Adam
+            r"([a-z]+)\s+([a-z]+)",            # Evan Wheeler
+            r"([a-z]+)\s+[a-z]+\.?\s+([a-z]+)",# Mark E. Johnston
+            r"([a-z]+)\s+([a-z]+\-[a-z]+)"     # Erica Kochi-Fabian
+        ]
+
+        def unique(str):
+            """Checks an alias for uniqueness; if it is already taken, alter it
+               (by append incrementing digits) until an available alias is found."""
+
+            n = 1
+            alias = str.lower()
+
+            # keep on looping until an alias becomes available.
+            # --
+            # WARNING: this isn't going to work at high volumes, since the alias
+            # that we return might be taken before we have time to do anything
+            # with it! This should logic should probably be moved to the
+            # initializer, to make the find/grab alias loop atomic
+            while klass.objects.filter(alias__iexact=alias).count():
+                alias = "%s%d" % (str.lower(), n)
+                n += 1
+
+            return alias
+
+        # try each pattern, returning as
+        # soon as we find something that fits
+        for pat in patterns:
+            m = re.match("^%s$" % pat, flat_name, re.IGNORECASE)
+            if m is not None:
+                g = m.groups()
+
+                # return single names as-is
+                # they might already be aliases
+                if len(g) == 1:
+                    alias = unique(g[0].lower())
+                    return (alias, g[0], "")
+
+                else:
+                    # return only the letters from
+                    # the first and last names
+                    alias = unique(g[0][0] + re.sub(r"[^a-zA-Z]", "", g[1]))
+                    return (alias.lower(), g[0], g[1])
+
+        # we have no idea what is going on,
+        # so just return the whole thing
+        alias = unique(re.sub(r"[^a-zA-Z]", "", flat_name))
+        return (alias.lower(), flat_name, "")
