7cc73657ebb7e6b6a8b028d80bf285f4b8ed33c9
Tobias McNulty <tobias@caktusgroup.com>
2010-12-02 10:49:27 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='growthmonitoring',
+    version='0.1',
+    license="BSD",
+
+    install_requires=['rapidsms'],
+
+    description='Growth monitoring application for RapidSMS',
+
+    author='Evan Wheeler, Tobias McNulty',
+    author_email='rapidsms@googlegroups.com',
+
+    url='https://github.com/mwana/rapidsms-growthmonitoring-app',
+    download_url='https://github.com/mwana/rapidsms-growthmonitoring-app/downloads',
+
+    package_dir={'': 'lib'},
+    packages=['growthmonitoring'],
+    include_package_data=True,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
